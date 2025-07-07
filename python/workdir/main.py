3997171bd8e5033dce57339e2aa53067b06f2c94
TNKNMKHK <=>
2025-07-07 16:55:14 +0900

    お知らせの表示

--- python/workdir/main.py
+++ python/workdir/main.py
@@ -2,6 +2,7 @@ import streamlit as st
 from login import login_form
 from pages.mypage import show_mypage
 from pages.report_stop import show_report_stop
+from pages.notice import show_notice 
 
 
 def main():
@@ -17,10 +18,10 @@ def main():
     tab1, tab2,tab3 = st.tabs(MenuList)
     
     with tab1:
+        show_notice()
         
     with tab2:
         show_mypage(UserName)
-        
         
     with tab3:
         show_report_stop()
---
+++ python/workdir/pages/notice.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+def show_notice():
+    """お知らせページを表示する関数"""
+    st.subheader("お知らせ")
+    st.write("ここにお知らせの内容が表示されます。")
