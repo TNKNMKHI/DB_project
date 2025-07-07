cefc325455080dc78b77fa1b9486352578064c0f
TNKNMKHK <=>
2025-07-07 16:33:02 +0900

    標準入力を削除

--- python/workdir/login.py
+++ python/workdir/login.py
@@ -67,8 +67,6 @@ def password_hash(password):
     # secretsモジュールを使ってランダムなソルトを生成
     salt = secrets.token_hex(16)
 
-    # 入力内容からユーザーのパスワードを取得
-    password = input("Enter your password: ")
 
     # ソルトとSHA-256アルゴリズムを使ってパスワードをハッシュ化
     hash_object = hashlib.sha256((password + salt).encode())
