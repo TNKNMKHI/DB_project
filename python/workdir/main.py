2ec320ad854d3e68f46bb399e2d090128ce38e0e
TNKNMKHK <=>
2025-07-07 16:52:14 +0900

    お知らせを一番最初に表示

--- python/workdir/main.py
+++ python/workdir/main.py
@@ -10,14 +10,19 @@ def main():
         return # ログインしていない場合はここで終了
 
     UserName = st.session_state["username"]
-    st.write(f"Welcome, {UserName}!")
+    st.markdown(f"### Welcome, {UserName}!")
 
     # タブでページ切り替え
-    tab1, tab2 = st.tabs(["マイページ", "報告停止"])
+    MenuList = ["お知らせ","マイページ", "出席停止生徒一覧"]
+    tab1, tab2,tab3 = st.tabs(MenuList)
     
     with tab1:
-        show_mypage(UserName)
+        
     with tab2:
+        show_mypage(UserName)
+        
+        
+    with tab3:
         show_report_stop()
 
 
